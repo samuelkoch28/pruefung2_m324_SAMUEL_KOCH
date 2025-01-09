0c2a62ae431e4fe84a27aa4aa74a0a0c4c3ec19c
Samuel Koch <samuel_koch@sluz.ch>
2025-01-09 14:26:16 +0100

    deleted index.js content

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-import { generate } from 'random-words';
-import chalk from 'chalk';
 export const sum = (a, b) => {
   return a + b;
 };
-
-const word = generate();
-console.log(
-  `Hello!!! 
-  The ${chalk.redBright('random word')} of today is: ${chalk.bgRed(word)}. 
-  The word length is ${sum(word.length, 0)}.`
-);
