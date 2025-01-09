b00f1b81d048328460df891953e449d31b48b62f
Samuel Koch <samuel_koch@sluz.ch>
2025-01-09 14:13:57 +0100

    Task 3: Fixed Linter Error

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ export const sum = (a, b) => {
 };
 
 const word = generate();
-const word2 = generate();
 console.log(
   `Hello!!! 
   The ${chalk.redBright('random word')} of today is: ${chalk.bgRed(word)}. 
